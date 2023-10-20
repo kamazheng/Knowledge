92de3e8752c066a7b22607be3f420da47f236802
kamazheng <49426393+kamazheng@users.noreply.github.com>
2023-10-20 08:31:01 +0800

    Add files via upload

---
+++ C#/Blazor/RoslynRuntimeCompile.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using System.Reflection;
+
+namespace RoslynCompileSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(
+                """
+                using System;
+
+                namespace RoslynCompileSample
+                {
+                    public class Writer
+                    {
+                        public void Write(string message)
+                        {
+                           Console.WriteLine(message);
+                        }
+                    }
+                }
+                """
+                );
+
+            string assemblyName = Path.GetRandomFileName();
+
+            MetadataReference[] references = AppDomain.CurrentDomain.GetAssemblies()
+                .Where(_ => !_.IsDynamic && !string.IsNullOrWhiteSpace(_.Location))
+                .Select(_ => MetadataReference.CreateFromFile(_.Location))
+                .Concat(new[]
+                {
+                    // add your app/lib specifics, e.g.:
+                    MetadataReference.CreateFromFile(typeof(Program).Assembly.Location)
+                })
+                .ToArray();
+
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                assemblyName,
+                syntaxTrees: new[] { syntaxTree },
+                references: references,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            using (var ms = new MemoryStream())
+            {
+                EmitResult result = compilation.Emit(ms);
+
+                if (!result.Success)
+                {
+                    IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
+                        diagnostic.IsWarningAsError ||
+                        diagnostic.Severity == DiagnosticSeverity.Error);
+
+                    foreach (Diagnostic diagnostic in failures)
+                    {
+                        Console.Error.WriteLine("{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
+                    }
+                }
+                else
+                {
+                    ms.Seek(0, SeekOrigin.Begin);
+                    Assembly assembly = Assembly.Load(ms.ToArray());
+
+                    Type type = assembly.GetType("RoslynCompileSample.Writer");
+                    object obj = Activator.CreateInstance(type);
+                    type.InvokeMember("Write",
+                        BindingFlags.Default | BindingFlags.InvokeMethod,
+                        null,
+                        obj,
+                        new object[] { "Hello World" });
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
